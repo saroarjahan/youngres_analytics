c133116a68ec309a1fe6bcc2f07f940108ba1b9e
Saroar Jahan <saroarjahan.bd@gmail.com>
2021-06-01 19:48:40 +0300

    Update Report.vue

--- src/components/pages/Report.vue
+++ src/components/pages/Report.vue
@@ -1,12 +1,7 @@
 <template>
     <div class="container">
-      <div class="loading" v-if="loading">
+      <div class="loading">
         <div class="centerScreen">
-          <div class="spinner-grow text-dark" role="status">
-            <span class="sr-only">Loading...</span>
-          </div>
-        </div>
-      </div>
         <div class="content Timesroman">
 
             <h3 class="title"> Game Report</h3>
@@ -15,85 +10,65 @@
 
             <p class="body">This section presents a brief report of overall games played by different groups. In total, <strong>{{GroupFilter.group_ids.length}}</strong> group (<strong> {{GroupFilter.group_ids[0].group_id}}</strong>) has participated with 100 students and played  <strong>{{result.length}}</strong> game and <strong>{{result[0].chapters.length}}</strong> chapters, and more than 120 events.  Below presents a summary of each chapter and the risk of polarization for students regarding their provided decision-making during their games play.</p>
 
-            <h2 class="title">Chapter 1: Psychological evaluation</h2>
+            
 
-            <p class="body">This chapter is played by <strong>{{GroupFilter.group_ids[0].group_id}}</strong> with participants <strong>{{decisions.length}}</strong> (60% boy, 40% girls) and 80  (60% boy, 40% girls). Group_1 has a polarization risk of 80%, and group_2 has 70%. The most polarization decision has come from event ID (2,5,7), and the lowest polarization score comes from event ID (1,3), details scores of each event shown in Fig 1 and Fig 2. The most polarized decision came from event 7: How will you behave your mother and 80% of student answer was 'I will shout'.
+         
+
+
+
+
+    
+
+            {{new_score}}
+
+            {{total_score}}
+
+  
+
+
+            <div v-for="(item, index) in [1593,4474,2377]" :key="index">
+
+            {{submitData(item)}} 
+
+            <!-- {{d_counts()}} -->
+
+            <!-- {{unique_decision_final}} -->
+
+
+            <h2 class="title">Chapter {{index+1}}: Psychological evaluation</h2>
+
+            <p class="body">This chapter is played by <strong>{{GroupFilter.group_ids[0].group_id}}</strong> with participants <strong>{{decisions.length}}</strong> (60% boy, 40% girls) and 80  (60% boy, 40% girls). Group_1 has a polarization risk of 80%, and group_2 has 70%. The most polarization decision has come from event ID (2,5,7), and the lowest polarization score comes from event ID (1,3), details scores of each event shown in Fig 1 and Fig 2. The most polarized decision came from event 7: How will you behave your mother and 80% of student answer was 'I will shout'.</p>
 
             <br>  
+             <v-chart :options="chartData" width="100%"/>
 
 
            Among this decision, 60% of boys answer showed polarization risk other while 30% of girls showed polarization risk. Besides among 9, 11, 12,13 polarization risk was 30%, 10%, 15% and 20%. This indicates nine years old are more prone to be polarized.
 
 
-          <br>
+             <br>
 
-          <div class="col-md-8">
-                    <v-chart :options="chartData" width="100%"/>
-                
+
+
             </div>
 
 
-<!--  {{result}}
-
-        {{result[0].chapters}} 
-
-
-        {{choiceList}} -->
-
-
-        <!-- {{unique_decision_final}} -->
-   <!--        {{GroupFilter.group_ids.length}}
-
-        {{GroupFilter.group_ids[0].group_id}} -->
-
-<!-- 
-        {{decisions.length}}
-
-
-        {{choiceList_name}}
-
-
-        {{getFilterHeader}} -->
-
-<!--         {{ decisions.sort(function (a, b) {
-  return a.eventCode - b.eventCode
-})}}
- -->
-
- <!-- {{unique_decision_final}} -->
-
-        <!-- {{annotation}} -->
-
-<!-- {{d_count}} -->
-
-<!-- {{emotion[0]}} -->
-
-{{new_score}}
-
-{{total_score}}
-
-
-{{chartData}}
-<!-- {{unique_decision_final_temp}}
-
-{{distinct_event_temp}} -->
-
-<!-- {{chartData}} -->
-
-<!-- {{distinct_event}} -->
-<!-- {{decisions}} -->
 
 
 
-        <div class="row" style="padding: 20px 0">
-            <div class="col-4 text-center">
-                <button class="btn btn-primary" @click="home">Home</button>
-            </div>
-            <div class="col-4 text-right">
-                <button class="btn btn-primary export" @click="exportOpt">Export</button>
-            </div>
-        </div>
-    </div>
+
+              <div class="row" style="padding: 20px 0">
+                  <div class="col-4 text-center">
+                      <button class="btn btn-primary" @click="home">Home</button>
+                  </div>
+                  <div class="col-4 text-right">
+                      <button class="btn btn-primary export" @click="exportOpt">Export</button>
+                  </div>
+              </div>
+          </div>
+
+          </div>
+      </div>
      </div>
 </template>
 
@@ -112,8 +87,6 @@
         },
         data: function(){
             return {
-                chartData: null,
-                loading: true,
                 choice: 'choice',
                 decisions: [],
                 max_choice: 0,
@@ -153,50 +126,9 @@
           const requestThree = axios.get("filters/student");
 
 
-          axios.all([requestOne, requestTwo, requestThree ]).then(axios.spread((...responses) => {
-            this.$store.state.games = responses[0].data.games;
-            this.GroupFilter = responses[1].data
-            this.filterStudent = responses[2].data
-
-            this.groupsList = this.GroupFilter.group_ids;
-            this.SelectGroupOne = this.$route.params.groupOne;
-            this.SelectGroupTwo = this.$route.params.groupTwo;
-            this.loadData();
-            this.submitData();
 
 
-          })).catch(errors => {
-            console.log(errors);
-          })
-
-
-          axios.get('descriptions/games')
-              .then(res => {
-                this.$store.state.games = JSON.parse(res.request.response).games;
-                this.loading = false;
-                this.loadData();
-                this.submitData();
-
-              });
-
-
-          this.$root.$on('loadFilterHeaderSingle', (Filter) => { // here you need to use the arrow function
-            this.getFilterHeader = {};
-            if(Filter.group !== null && Filter.group !== undefined) {
-              if(Filter.student === null )
-                delete  Filter.student;
-
-              this.getFilterHeader = Filter;
-
-            } else if(Filter.student !== null && Filter.student !== undefined) {
-
-              if(Filter.group === null )
-                delete  Filter.group;
-
-              this.getFilterHeader = Filter;
-            }
-
-            this.submitData();
+    
 
 
           });
@@ -204,6 +136,11 @@
         }
         ,
         methods: {
+            sh(item){
+
+              return item;
+
+            },
             exportOpt(){
                 window.print();
             },
@@ -219,36 +156,25 @@
             },
  
  
-            submitData(){
+            submitData(item){
 
                 this.loading = true;
                 if(this.getFilterHeader !== null && JSON.stringify(this.getFilterHeader) !== JSON.stringify({})) {
 
-                  axios.get("decision?gameCode=" + this.game + "&gameVersion=" + this.version + "&chapterCode=" + this.chapter, {headers: {filters: JSON.stringify(this.getFilterHeader)}}).then(res => {
+                  axios.get("decision?gameCode=" + this.game + "&gameVersion=" + this.version + "&chapterCode=" + item, {headers: {filters: JSON.stringify(this.getFilterHeader)}}).then(res => {
                     this.decisions = res.data.decisions;
                     this.dataAnalysis();
-                    this.barChartLoad();
+                    // this.d_counts();
+                    // this.barChartLoad();
 
                   });
 
                 }else {
-                  axios.get("decision?gameCode=" + this.game + "&gameVersion=" + this.version + "&chapterCode=" + this.chapter).then(res => {
+                  axios.get("decision?gameCode=" + this.game + "&gameVersion=" + this.version + "&chapterCode=" + item).then(res => {
                     this.decisions = res.data.decisions;
                     this.dataAnalysis();
-                    let i =0;
-                    this.chartDATA = [];
-                    this.unique_decision_final.forEach(value => {
-                      this.chartDATA.push({
-                        name: 'bar'+ i++,
-                        type: 'bar',
-                        stack: 'one',
-                        barWidth: 20,
-                        data: value.choice
-                      });
-                    });
-
-                    this.d_counts();
-                    this.loading = false;
+                    // this.d_counts();
+                    // this.barChartLoad();
                   });
                 }
             },
@@ -365,95 +291,49 @@
               this.unique_decision_final = choice;
               this.choiceList = eventList;
             },
-
             barChartLoad(){
 
+                  this.chartData = {    
+                          tooltip: {
+                              trigger: 'axis',
+                              axisPointer: {
+                                  type: 'shadow'
+                              }
+                          },
+                          legend: {
+                              data: ['2011年', '2012年']
+                          },
+                          grid: {
+                              left: '3%',
+                              right: '4%',
+                              bottom: '3%',
+                              containLabel: true
+                          },
+                          xAxis: {
+                              type: 'value',
+                              boundaryGap: [0, 0.01]
+                          },
+                          yAxis: {
+                              type: 'category',
+                              data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
+                          },
+                          series: [
+                              {
+                                  name: '2011年',
+                                  type: 'bar',
+                                  data: [18203, 23489, 29034, 104970, 131744, 630230]
+                              },
+                              {
+                                  name: '2012年',
+                                  type: 'bar',
+                                  data: [19325, 23438, 31000, 121594, 134141, 681807]
+                              }
 
-                if(this.choice === 'choice') {
-                    this.chartData = {
-                        backgroundColor: '#fff',
-                        tooltip: {
-                            formatter: function (params) {
-                              console.log(params)
-                                return params.data.event +': '+params.data.description+'<br>Answer: “'+params.data.name+'” <br/>Selected by: '+
-                                    params.value+'% of the students';
-                            }
-                        },
-                        calculable: true,
-                        xAxis: {
-                            data: this.choiceList,
-                            axisLine: {onZero: true},
-                            splitLine: {show: true},
-                            splitArea: {show: false}
-                        },
-                        yAxis: {
-                            max: 100,
-                            splitArea: {show: false},
-                            axisLabel:{
-                                formatter: '{value}%'
-                            }
-                        },
-                        series: this.chartDATA
+                          ]
                     };
-                }else{
-                    this.chartData = {
-                        xAxis: {
-                            scale: true,
-                            data: this.distinct_event_temp
-                        },
-                        yAxis: {
-                            scale: true,
-                            min: 0,
-                            max: 100,
-                            axisLabel:{
-                                formatter: '{value} Secs'
-                            }
-                        },
-                        tooltip: {
-                            trigger: 'axis',
-                            axisPointer:{
-                                type: 'shadow'
-                            },
-                            formatter: function (params) {
-                                let sum =0;
-                                let mean;
-                                let std;
-                                let Event;
-                                let description;
-
-                                params.forEach((value) => {
-                                    sum = sum + parseFloat(value.data[1]);
-                                    description = value.data[2];
-                                    Event = value.name;
-                                });
-                                mean = (sum / params.length ).toFixed(2);
-
-                                sum = 0;
-                                params.forEach((value) => {
-                                    let x = (parseFloat(value.data[1]) - mean);
-                                    sum = sum + (x*x);
-                                });
-                                std = Math.sqrt(sum / (params.length));
-
-                                return Event+' : '+description+'<br/>Avg: ' + mean + ' secs.<br/>Std: ' + std.toFixed(2)+' secs.';
-                            }
-                        },
-                        series: [
-                            {
-                                type: 'scatter',
-                                data: this.unique_decision_final_temp,
-                            }
-                        ]
-                    };
-                }
-
-              this.loading = false;
             },
-      
 
       
-
-
             d_counts(){
                   this.unique_decision_final.forEach(( message) =>{   
                      message.choice.forEach(( new_m) =>{
@@ -504,6 +384,8 @@
                           });
 
                   this.total_score.push(tpo,tne,tnu,tcom);
+
+
             },
 
 
@@ -555,32 +437,7 @@
             background: #848484;
         }
     }
-    .loading {
-      position: fixed;
-      top: 0;
-      left: 0;
-      background: #0000003d;
-      height: 100%;
-      width: 100%;
-      z-index: 99999;
-      .centerScreen {
-        position: fixed;
-        width: 100%;
-        transform: translate(50%, 50%);
-        height: 100%;
-        margin-left: -2.5rem;
-        margin-top: -2.5rem;
-
-        .spinner-grow {
-          width: 5rem;
-          height: 5rem;
-        }
-
-        .text-dark {
-          color: #e35219 !important;
-        }
-      }
-    }
+    
     .echarts {width: 100% !important;}
 
     h2.title[data-v-16f032e2] {
